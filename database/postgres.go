624edcdb29fd37bb1038d51bc665b41c1028d682
Adam Pickering <adam.pickering@goteleport.com>
2026-04-08 12:46:53 -0600

    Switch postgres to using official postgres dockerhub container

--- database/postgres.go
+++ database/postgres.go
@@ -9,22 +9,25 @@ import (
 	"dbtest/teleport"
 )
 
-const postgresDockerfile = `FROM ubuntu:24.04
+const postgresDockerfile = `FROM postgres:16
 
-# Installing curl is critical because it sets up a directory we
-# later use for certificates
-RUN apt-get update && apt-get install -y curl vim postgresql
+ENV POSTGRES_PASSWORD=password
 
 COPY postgres.cas /pg-certs/postgres.cas
 COPY postgres.crt /pg-certs/postgres.crt
 COPY postgres.key /pg-certs/postgres.key
-RUN chown postgres /pg-certs/postgres.cas /pg-certs/postgres.crt /pg-certs/postgres.key
+RUN chown postgres:postgres /pg-certs/postgres.cas /pg-certs/postgres.crt /pg-certs/postgres.key \
+    && chmod 600 /pg-certs/postgres.key
 
-RUN sed -i "/^# configuration parameter, or via the -i or -h command line switches./a\hostssl all             all             ::/0                    cert\nhostssl all             all             0.0.0.0/0               cert" /etc/postgresql/16/main/pg_hba.conf
-RUN sed -i "\$a\ssl = on\nssl_cert_file = '/pg-certs/postgres.crt'\nssl_key_file = '/pg-certs/postgres.key'\nssl_ca_file = '/pg-certs/postgres.cas'" /etc/postgresql/16/main/postgresql.conf
-RUN sed -i "/^#listen_addresses = 'localhost'/i\listen_addresses = '*'" /etc/postgresql/16/main/postgresql.conf
+COPY pg_hba.conf /etc/postgresql/pg_hba.conf
 
 EXPOSE 5432
+`
+
+const postgresHBAConf = `# TYPE  DATABASE  USER  ADDRESS    METHOD
+local   all       all              trust
+hostssl all       all   ::/0       cert
+hostssl all       all   0.0.0.0/0  cert
 `
 
 type Postgres struct {
@@ -43,6 +46,10 @@ func (p *Postgres) Up() error {
 		return fmt.Errorf("writing Dockerfile: %w", err)
 	}
 
+	if err := os.WriteFile(filepath.Join(dir, "pg_hba.conf"), []byte(postgresHBAConf), 0644); err != nil {
+		return fmt.Errorf("writing pg_hba.conf: %w", err)
+	}
+
 	fmt.Println("generating postgres certificates...")
 	if err := teleport.AuthSign(dir, "db", p.Config.ClusterName+"-"+p.Config.Name, "postgres", "2160h"); err != nil {
 		return fmt.Errorf("generating postgres certificates: %w", err)
@@ -58,8 +65,15 @@ func (p *Postgres) Up() error {
 		Network: p.Config.ClusterName,
 		Name:    imageName,
 		Image:   imageName,
-		User:    "postgres",
-		Cmd:     []string{"/usr/lib/postgresql/16/bin/postgres", "--config-file=/etc/postgresql/16/main/postgresql.conf"},
+		Cmd: []string{
+			"postgres",
+			"-c", "ssl=on",
+			"-c", "ssl_cert_file=/pg-certs/postgres.crt",
+			"-c", "ssl_key_file=/pg-certs/postgres.key",
+			"-c", "ssl_ca_file=/pg-certs/postgres.cas",
+			"-c", "hba_file=/etc/postgresql/pg_hba.conf",
+			"-c", "listen_addresses=*",
+		},
 	}); err != nil {
 		return fmt.Errorf("starting postgres container: %w", err)
 	}
